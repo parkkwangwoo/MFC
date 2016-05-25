9409cfedd352c5cc3d9885f695cd31fe4ce6241f
ParkKwangWoo <qkreheh104@naver.com>
2016-05-26 00:52:24 +0900

    1

---
+++ MFCApplication2/ChildView.cpp
@@ -0,0 +1,78 @@
+
+// ChildView.cpp : CChildView 클래스의 구현
+//
+
+#include "stdafx.h"
+#include "MFCApplication2.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_COMMAND(ID_32772, &CChildView::On32772)
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(NULL, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), NULL);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	CRect rect;
+	GetClientRect(&rect);
+	CBrush brush(HS_CROSS, RGB(234, 234, 234));
+	dc.SelectObject(&brush);
+	dc.Rectangle(&rect);
+
+	
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	
+	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
+}
+
+
+
+void CChildView::On32772()
+{
+	CPaintDC dc(this);
+	
+		CBitmap bitmap1;
+		bitmap1.LoadBitmap(IDB_BITMAP1);
+		BITMAP bmpinfo1;
+		bitmap1.GetBitmap(&bmpinfo1);
+		CDC dcmem1;
+		dcmem1.CreateCompatibleDC(&dc);
+		dcmem1.SelectObject(&bitmap1);
+		dc.BitBlt(20, 20, bmpinfo1.bmWidth, bmpinfo1.bmHeight, &dcmem1, 0, 0, SRCCOPY);
+	
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+}
---
+++ MFCApplication2/resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplication2.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_MFCApplication2TYPE         130
+#define IDB_BITMAP1                     310
+#define ID_MENU_32771                   32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_MENU_32778                   32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32782
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
